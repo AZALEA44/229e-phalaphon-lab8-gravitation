dd5c21414cdab96f3e71f7fe6c23622d3bbbbe32
AZALEA44 <midnightbestever@gmail.com>
2025-03-13 10:30:03 +0700

    scriptbackup

--- Assets/Script/Gravity.cs
+++ Assets/Script/Gravity.cs
@@ -1,16 +1,47 @@
+using NUnit.Framework;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class Gravity : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    Rigidbody rb;
+    const float G = 0.00674f;
+    public static List<Gravity> gravityObjectList;
+
+    private void Awake()
     {
+        rb = GetComponent<Rigidbody>();
+
+        if (gravityObjectList == null)
+        {
+            gravityObjectList = new List<Gravity>();
+        }
+
+        gravityObjectList.Add(this);
+        
         
     }
 
-    // Update is called once per frame
-    void Update()
+    private void FixedUpdate()
     {
-        
+        foreach (var obj in gravityObjectList)
+        {
+            if (obj != this)
+            {
+                Attract(obj);
+            }
+        }
+    }
+
+    void Attract(Gravity other)
+    {
+        Rigidbody otherRb = other.rb;
+
+        Vector3 direction = rb.position - otherRb.position;
+        float distance = direction.magnitude;
+        float forceMagnitude = G * ((rb.mass * otherRb.mass) / Mathf.Pow(distance, 2));
+        Vector3 finalForce = forceMagnitude * direction.normalized;
+
+        otherRb.AddForce(finalForce);
     }
 }
